edec9f74139a34da3e8615b52aef7cd6020ae505
Michael Demidenko <36335002+demidenm@users.noreply.github.com>
2024-05-02 09:05:46 -0700

    scripts for MID rt vs non-rt models

---
+++ scripts/firstlvl_runs.py
@@ -0,0 +1,149 @@
+import warnings
+warnings.filterwarnings("ignore")
+import sys
+import os
+import argparse
+import pandas as pd
+from glob import glob
+from nilearn.glm.first_level import FirstLevelModel
+import matplotlib.pyplot as plt
+from nilearn.plotting import plot_design_matrix
+
+
+# Getpath to Stage2 scripts
+project_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(project_dir)
+from scripts.model_designmat_regressors import create_design_mid, pull_regressors, fixed_effect
+
+parser = argparse.ArgumentParser(description="Script to run first level task models w/ nilearn")
+
+parser.add_argument("--sample", help="sample type, abcd, AHRB, MLS?")
+parser.add_argument("--sub", help="subject name, sub-XX, include entirety with 'sub-' prefix")
+parser.add_argument("--task", help="task type -- e.g., mid, reward, etc")
+parser.add_argument("--ses", help="session, include the session type without prefix, e.g., 1, 01, baselinearm1")
+parser.add_argument("--numvols", help="The number of volumes for BOLD file, e.g numeric")
+parser.add_argument("--boldtr", help="the tr value for the datasets in seconds, e.g. .800, 2.0, 3.0")
+parser.add_argument("--beh_path", help="Path to the behavioral (.tsv) directory/files for the task")
+parser.add_argument("--fmriprep_path", help="Path to the output directory for the fmriprep output")
+parser.add_argument("--mask", help="path the to a binarized brain mask (e.g., MNI152 or "
+                                   "constrained mask in MNI space, spec-network",
+                    default=None)
+parser.add_argument("--mask_label", help="label for mask, e.g. mni152, subtresh, suprathresh, yeo-network",
+                    default=None)
+parser.add_argument("--output", help="output folder where to write out and save information")
+args = parser.parse_args()
+
+# Now you can access the arguments as attributes of the 'args' object.
+sample = args.sample
+subj = args.sub
+task = args.task
+ses = args.ses
+numvols = int(args.numvols)
+boldtr = float(args.boldtr)
+beh_path = args.beh_path
+fmriprep_path = args.fmriprep_path
+brainmask = args.mask
+mask_label = args.mask_label
+scratch_out = args.output
+
+contrasts = [
+    # anticipatory contrasts for cue-model
+    'LRew-Neut', 'ARew-Neut', 'LPun-Neut', 'APun-Neut',
+    # feedback contrasts
+    'ARewHit-ARewMiss', 'LRewHit-LRewMiss', 'APunHit-APunMiss',
+    'LPunHit-LPunMiss', 'LRewHit-LNeutHit'
+]
+contrast_labs = {
+    # Anticipation
+    'LRew-Neut': 'LgReward - Triangle',
+    'ARew-Neut': 'LgReward + Small Reward - Triangle',
+    'LPun-Neut': 'LgPun - Triangle',
+    'APun-Neut': 'LgPun + Small Pun - Triangle',
+
+    # Feedback
+    'ARewHit-ARewMiss': 'LgReward_hit + SmallReward_Hit - LgReward_miss + SmallReward_miss',
+    'LRewHit-LRewMiss': 'LgReward_hit - LgReward_miss',
+    'APunHit-APunMiss': 'LgPun_hit + SmallPun_Hit - LgPun_miss + SmallPun_miss',
+    'LPunHit-LPunMiss': 'LgPun_hit - LgPun_miss',
+    'LRewHit-LNeutHit': 'LgReward_hit - Triangle_hit',
+
+}
+
+contrast_weights = {
+    # Anticipation
+    'LR-Neut': {'LgReward': 1,
+                'NoMoneyStake': -1},
+    'AR-Neut': {'LgReward': 1, 'SmallReward': 1,
+                'NoMoneyStake': -2},
+    'LP-Neut': {'LgPun': 1,
+                'NoMoneyStake': -1},
+    'AP-Neut': {'LgPun': 1, 'SmallPun': 1,
+                'NoMoneyStake': -2},
+
+    # Feedback
+    'ARewHit-ARewMiss': {'LgReward_hit': 1, 'SmallReward_Hit': 1,
+                         'LgReward_miss': -1, 'SmallReward_miss': -1},
+    'LRewHit-LRewMiss': {'LgReward_hit': 1,
+                         'LgReward_miss': -1},
+    'APunHit-APunMiss': {'LgPun_hit': 1, 'SmallPun_Hit': 1,
+                         'LgPun_miss': -1, 'SmallPun_miss': 1},
+    'LPunHit-LPunMiss': {'LgPun_hit': 1,
+                         'LgPun_miss': -1},
+    'LRewHit-LNeutHit': {'LgReward_hit': 1,
+                         'Triangle_hit': -1}
+}
+
+fwhm = 5
+runs = ['01', '02']
+
+for run in runs:
+    print(f'\tStarting {subj} {run}.')
+    # import behavior events .tsv from data path
+    events_df = pd.read_csv(f'{beh_path}/{subj}/ses-{ses}/func/{subj}_ses-{ses}_task-{task}_run-{run}_events.tsv',
+                            sep='\t')
+
+    # get path to confounds from fmriprep, func data + mask, set image path
+    conf_path = f'{fmriprep_path}/{subj}/ses-{ses}/func/{subj}_ses-{ses}_task-{task}_run-{run}' \
+            f'_desc-confounds_timeseries.tsv'
+    nii_path = glob(
+    f'{fmriprep_path}/{subj}/ses-{ses}/func/{subj}_ses-{ses}_task-{task}_run-{run}'
+    f'_space-MNI152NLin2009cAsym_res-2_desc-preproc_bold.nii.gz')[0]
+    print('\t\t 1/3 Create Regressors & Design Matrix for GLM')
+    # get list of regressors
+    # run to create design matrix
+    conf_regressors = pull_regressors(confound_path=conf_path, regressor_type='opt2')
+
+    for model in [None, 'rt']:
+        design_matrix = create_design_mid(events_df=events_df, bold_tr=boldtr, num_volumes=numvols,
+                                          conf_regressors=conf_regressors, rt_model=model,
+                                          hrf_model='spm', stc=False)
+        # save design mat
+        plt.ioff()
+        plot_design_matrix(design_matrix)
+        plt.savefig(f'{scratch_out}/{subj}_ses-{ses}_task-{task}_run-{run}_mod-Cue-{model}_design-mat.png')
+
+        print('\t\t 2/3 Mask Image, Fit GLM model ar1 autocorrelation')
+        # using ar1 autocorrelation (FSL prewhitening), drift model
+        fmri_glm = FirstLevelModel(subject_label=subj, mask_img=brainmask,
+                                   t_r=boldtr, smoothing_fwhm=fwhm,
+                                   standardize=False, noise_model='ar1', drift_model=None, high_pass=None
+                                   )
+        # Run GLM model using set paths and calculate design matrix
+        run_fmri_glm = fmri_glm.fit(nii_path, design_matrices=design_matrix)
+        print('\t\t 3/3: From GLM model, create/save contrast beta/variance maps to output path')
+        for con_name, con in contrast_labs.items():
+            # calc beta
+            beta_name = f'{scratch_out}/{subj}_ses-{ses}_task-{task}_run-{run}_mod-Cue-{model}_stat-beta.nii.gz'
+            beta_est = run_fmri_glm.compute_contrast(con, output_type='effect_size')
+            beta_est.to_filename(beta_name)
+            # Calc: variance
+            var_name = f'{scratch_out}/{subj}_ses-{ses}_task-{task}_run-{run}_mod-Cue-{model}_stat-var.nii.gz'
+            var_est = run_fmri_glm.compute_contrast(con, output_type='effect_variance')
+            var_est.to_filename(var_name)
+
+print("Running Fixed effect model -- precision weight of runs for each contrast")
+
+for model in [None, 'rt']:
+    fixed_effect(subject=subj, session=ses, task_type=task,
+                 contrast_list=contrasts, firstlvl_indir=scratch_out, fixedeffect_outdir=scratch_out,
+                 model_lab=model, save_beta=True, save_var=True, save_tstat=False)
---
+++ scripts/model_designmat_regressors.py
@@ -0,0 +1,223 @@
+import os
+import pandas as pd
+import numpy as np
+from nilearn.glm.first_level import make_first_level_design_matrix
+from nilearn.glm import compute_fixed_effects
+from glob import glob
+
+
+def eff_estimator(desmat, contrast_matrix):
+    """
+    [code by Jeanette Mumford]
+    Estimates the efficiency for a given design matrix for each contrast (row) of a contrast matrix
+    input:
+      desmat: design matrix
+      contrast_matrix: contrast matrix where each row the matrix corresponds to a contrast.
+          Must have same number of columns as desmat
+    output:
+      returns an efficiency for each contrast, separately.  Given in same order as contrast order in
+        contrast_matrix
+    """
+    cov_mat = contrast_matrix @ np.linalg.inv(desmat.transpose()@desmat) @ contrast_matrix.transpose()
+    var_vec = np.diag(cov_mat)
+    return 1/var_vec
+
+
+def pull_regressors(confound_path: str, regressor_type: str = 'opt1') -> pd.DataFrame:
+    """
+    This function is compatible with the *confounds_timeseries.tsv file exported by fMRIprep
+    When calling this function, provide the path to the confounds file for each subject (and run) and select
+    the type of confounds to pull (opt_1 to opt_5).
+    The functions returns a pandas dataframe with the extract confounds.
+
+    :param confound_path: path to the *counfounds_timeseries.tsv
+    :param regressor_type: Confound option from list 'conf_opt1','conf_opt2','conf_opt3','conf_opt4'
+        'opt1': cosine_00 to cosine_03
+        'opt2': opt1 + tran x, y, z & rot x, y, z
+        'opt3': opt2 + trans x, y, z and rot x, y, z derivatives
+        'opt4': opt3 + a_comp_cor 0:7 (top 8 components)
+        'opt5': opt4 + motion outliers in confounds file
+    :return: list of confound regressors
+    """
+    if not os.path.exists(confound_path):
+        raise ValueError("Confounds file path not found. Check if {} exists".format(confound_path))
+
+    confound_df = glob(confound_path)[0]
+    confound_df = pd.read_csv(confound_df, sep='\t', na_values=['n/a']).fillna(0)
+
+    # Setting up dictionary from which to pull confound list
+    if regressor_type != 'opt4':
+        confound_dict = {
+            "opt1": ['cosine00', 'cosine01', 'cosine02', 'cosine03'],
+            "opt2": ['cosine00', 'cosine01', 'cosine02', 'cosine03',
+                     'trans_x', 'trans_y', 'trans_z', 'rot_x', 'rot_y', 'rot_z',
+                     'trans_x_derivative1', 'trans_y_derivative1', 'trans_z_derivative1',
+                     'rot_x_derivative1', 'rot_y_derivative1', 'rot_z_derivative1'],
+            "opt3": ['cosine00', 'cosine01', 'cosine02', 'cosine03',
+                     'trans_x', 'trans_y', 'trans_z', 'rot_x', 'rot_y', 'rot_z',
+                     'trans_x_derivative1', 'trans_y_derivative1', 'trans_z_derivative1',
+                     'rot_x_derivative1', 'rot_y_derivative1', 'rot_z_derivative1',
+                     "a_comp_cor_00", "a_comp_cor_01", "a_comp_cor_02", "a_comp_cor_03", "a_comp_cor_04",
+                     "a_comp_cor_05", "a_comp_cor_06", "a_comp_cor_07"]
+        }
+
+    else:
+        confound_dict = {
+            "opt1": ['cosine00', 'cosine01', 'cosine02', 'cosine03'],
+            "opt2": ['cosine00', 'cosine01', 'cosine02', 'cosine03',
+                     'trans_x', 'trans_y', 'trans_z', 'rot_x', 'rot_y', 'rot_z',
+                     'trans_x_derivative1', 'trans_y_derivative1', 'trans_z_derivative1',
+                     'rot_x_derivative1', 'rot_y_derivative1', 'rot_z_derivative1'],
+            "opt3": ['cosine00', 'cosine01', 'cosine02', 'cosine03',
+                     'trans_x', 'trans_y', 'trans_z', 'rot_x', 'rot_y', 'rot_z',
+                     'trans_x_derivative1', 'trans_y_derivative1', 'trans_z_derivative1',
+                     'rot_x_derivative1', 'rot_y_derivative1', 'rot_z_derivative1',
+                     "a_comp_cor_00", "a_comp_cor_01", "a_comp_cor_02", "a_comp_cor_03", "a_comp_cor_04",
+                     "a_comp_cor_05", "a_comp_cor_06", "a_comp_cor_07"]
+        }
+
+        motion_outlier_columns = confound_df.filter(regex='motion_outlier')
+        # append the motion outlier columns to in dict to opt3  as opt5
+        confound_dict['opt4'] = confound_dict['opt3'] + list(motion_outlier_columns.columns)
+
+    if 'cosine03' not in confound_df.columns:
+        confound_dict[regressor_type].remove('cosine03')
+
+    return pd.DataFrame(confound_df[confound_dict[regressor_type]])
+
+
+def create_design_mid(events_df: pd.DataFrame, bold_tr: float, num_volumes: int, conf_regressors: pd.DataFrame,
+                      rt_model: str = None, hrf_model: str = 'spm', stc: bool = False) -> pd.DataFrame:
+    """
+    Creates a design matrix for each run with 5 anticipation 10 feedback conditions and
+    specified regressors, such as cosine, motion and/or acompcor from fmriprep confounds.tsv
+
+    :param events_df: this is the pandas dataframe for the events for the MID task
+    :param bold_tr: TR for the BOLD volume,
+    :param num_volumes: volumes in the BOLD
+    :param conf_regressors: dataframe of nuisance regressors from def(regressors)
+    :param rt_model: rt or None, default none
+    :param hrf_model: select hrf model for design matrix, default spm
+    :param stc: whether slice time correction was ran. To adjust the onsets/frame times in design matrix.
+            Default False, alt True
+    :return: returns a design matrix for specified model
+    """
+
+    # create a delinated hit v miss column so it is more clear
+    new_feedback_label = 'Feedback.Response'
+    events_df[new_feedback_label] = np.where(events_df['prbacc'] == 1.0,
+                                             events_df['Condition'] + '_hit',
+                                             events_df['Condition'] + '_miss')
+    if rt_model is 'rt':
+        try:
+            # concat  cue onset/duration + feedback onset/duration + probe regressors
+            conditions = pd.concat([events_df.loc[:, "Condition"],
+                                    events_df.loc[:, "Feedback.Response"],
+                                    pd.Series(["probe"] * len(events_df[['OverallRT', 'Probe.OnsetTime']])),
+                                    pd.Series(["probe_rt"] * len(events_df[['OverallRT', 'Probe.OnsetTime']].dropna()))
+                                    ], ignore_index=True)
+            onsets = pd.concat([events_df.loc[:, 'Cue.OnsetTime'],
+                                events_df.loc[:, "Feedback.OnsetTime"],
+                                events_df.loc[:, "Probe.OnsetTime"],
+                                events_df[['OverallRT', 'Probe.OnsetTime']].dropna()['Probe.OnsetTime']
+                                ], ignore_index=True)
+            duration = pd.concat([events_df.loc[:, 'Cue.Duration'],
+                                  events_df.loc[:, "FeedbackDuration"],
+                                  events_df.loc[:, "Probe.Duration"],
+                                  # convert ms RT times to secs to serve as duration
+                                  (events_df[['OverallRT', 'Probe.OnsetTime']].dropna()['OverallRT']) / 1000
+                                  ], ignore_index=True)
+
+            # create pandas df with events
+            design_events = pd.DataFrame({
+                'trial_type': conditions,
+                'onset': onsets,
+                'duration': duration
+            })
+        except Exception as e:
+            print("When creating RT design matrix, an error occurred: ", e)
+
+    elif rt_model is None:
+        # concat only cue onset/duration + feedback onset + duration
+        conditions = pd.concat([events_df.loc[:, "Condition"], events_df.loc[:, new_feedback_label]],
+                               ignore_index=True)
+        onsets = pd.concat([events_df.loc[:, 'Cue.OnsetTime'], events_df.loc[:, "Feedback.OnsetTime"]],
+                           ignore_index=True)
+        duration = pd.concat([events_df.loc[:, 'Cue.Duration'], events_df.loc[:, "FeedbackDuration"]],
+                             ignore_index=True)
+        # create pandas df with events
+        design_events = pd.DataFrame({'trial_type': conditions,
+                                      'onset': onsets,
+                                      'duration': duration})
+    else:
+        print("RT model should be None or True")
+
+    # Using the BOLD tr and volumes to generate the frame_times: acquisition time in seconds
+    frame_times = np.arange(num_volumes) * bold_tr
+    if stc:
+        # default modulation == '1'. Offset the times due to slice time correction, see blog post:
+        # https://reproducibility.stanford.edu/slice-timing-correction-in-fmriprep-and-linear-modeling /
+        frame_times += bold_tr / 2
+
+    design_matrix_mid = make_first_level_design_matrix(
+        frame_times=frame_times,
+        events=design_events,
+        hrf_model=hrf_model,
+        drift_model=None,
+        add_regs=conf_regressors
+        )
+
+    return design_matrix_mid
+
+
+def fixed_effect(subject: str, session: str, task_type: str,
+                 contrast_list: list, firstlvl_indir: str, fixedeffect_outdir: str,
+                 model_lab: str, save_beta=False, save_var=False, save_tstat=True):
+    """
+    This function takes in a subject, task label, set of computed contrasts using nilearn,
+    the path to contrast estimates (beta maps), the output path for fixed effec tmodels and
+    specification of types of files to save, the beta estimates, associated variance and t-stat (which is calculated
+    based on beta/variance values)
+    Several path indices are hard coded, so should update as see fit
+    e.g., '{sub}_ses-{ses}_task-{task}_effect-fixed_contrast-{c}_stat-effect.nii.gz'
+    :param subject: string-Input subject label, BIDS leading label, e.g., sub-01
+    :param session: string-Input session label, BIDS label e.g., ses-1
+    :param task_type: string-Input task label, BIDS label e.g., mid
+    :param contrast_list: list of contrast types that are saved from first level
+    :param model_lab: complete string of model permutation, e.g., 'fwhm-4_mot-opt1_mod-AntMod'
+    :param firstlvl_indir: string-location of first level output files
+    :param fixedeffect_outdir: string-location to save fixed effects
+    :param save_beta: Whether to save 'effects' or beta values, default = False
+    :param save_var: Whether to save 'variance' or beta values, default = False
+    :param save_tstat: Whether to save 'tstat', default = True
+    :return: nothing return, files are saved
+    """
+    for contrast in contrast_list:
+        print(f"\t\t\t Creating weighted fix-eff model for contrast: {contrast}")
+        betas = sorted(glob(f'{firstlvl_indir}/{subject}_ses-{session}_task-{task_type}_run-*_'
+                            f'contrast-{contrast}_mod-Cue-{model_lab}_stat-beta.nii.gz'))
+        var = sorted(glob(f'{firstlvl_indir}/{subject}_ses-{session}_task-{task_type}_run-*_'
+                          f'contrast-{contrast}_mod-Cue-{model_lab}_stat-var.nii.gz'))
+
+        # conpute_fixed_effects options
+        # (1) contrast map of the effect across runs;
+        # (2) var map of between runs effect;
+        # (3) t-statistic based on effect of variance;
+        fix_effect, fix_var, fix_tstat = compute_fixed_effects(contrast_imgs=betas,
+                                                               variance_imgs=var,
+                                                               precision_weighted=True)
+        if not os.path.exists(fixedeffect_outdir):
+            os.makedirs(fixedeffect_outdir)
+            print("Directory created:", fixedeffect_outdir)
+        if save_beta:
+            fix_effect_out = f'{fixedeffect_outdir}/{subject}_ses-{session}_task-{task_type}_' \
+                             f'contrast-{contrast}_mod-Cue-{model_lab}_stat-effect.nii.gz'
+            fix_effect.to_filename(fix_effect_out)
+        if save_var:
+            fix_var_out = f'{fixedeffect_outdir}/{subject}_ses-{session}_task-{task_type}_' \
+                          f'contrast-{contrast}_mod-Cue-{model_lab}_stat-var.nii.gz'
+            fix_var.to_filename(fix_var_out)
+        if save_tstat:
+            fix_tstat_out = f'{fixedeffect_outdir}/{subject}_ses-{session}_task-{task_type}_' \
+                            f'contrast-{contrast}_mod-Cue-{model_lab}_stat-tstat.nii.gz'
+            fix_tstat.to_filename(fix_tstat_out)
